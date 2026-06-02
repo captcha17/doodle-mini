c8ceeeb795e7b15889771fcdd7b4774a8f0cd264
dmitryrakushev <rakushevdima@gmail.com>
2026-06-02 13:44:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Doodle Mini (Meeting Scheduling Service)
+
+Meeting scheduling platform using Spring Boot and Java technologies
+
+## Overview
+
+The service enable users to manage their time slots, schedule meetings, and view their custom calendar availability.
+
+## Features
+
+- Users are able to define available slots, which can later be converted into meetings
+- Each user have a personal calendar where their time is managed
+- A slot can be booked as a meeting with a specific title and participants
+- The system supports querying free or busy slots, providing an aggregated view for a selected time frame
+
+## API 
+Coming soon
+
---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "com.doodle"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
